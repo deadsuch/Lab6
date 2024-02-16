d899e33d7cafd7d565499787f6a7938b0e4d4241
rere <your_nekotyan@mail.ru>
2024-02-16 17:24:25 +0300

    Rounding NoteColor

--- app/src/main/java/ui/components/NoteColor.kt
+++ app/src/main/java/ui/components/NoteColor.kt
@@ -1,13 +1,16 @@
 package ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -16,8 +19,18 @@ fun NoteColor(){
     Box(
         modifier = Modifier
             .size(40.dp)
+            .background(Color.Yellow)
             .clip(CircleShape)
             .background(Color.Red)
+
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+
+                ),
+                CircleShape
+            )
     )
 }
 
